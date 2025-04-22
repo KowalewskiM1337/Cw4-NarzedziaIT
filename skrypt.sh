0873d32444c5325287d77f6e5f7fa009c406e878
Michał <kowalewskim444@gmail.com>
2025-04-22 15:26:45 +0200

    Dodanie opcji --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,15 @@
 if [[ $1 == "--date" ]]; then
 	echo $(date)
 fi
+
+i=1
+if [[ $1 == "--logs" ]] && [[ $2 == "" ]]; then
+	while [ $i -le 100 ]
+	do
+		touch "log$i.txt"
+		echo "log$i.txt" >> "log$i.txt"
+		echo "skrypt.sh" >> "log$i.txt"
+		echo $(date) >> "log$i.txt"
+		i=$(( $i + 1 ))
+	done
+fi
