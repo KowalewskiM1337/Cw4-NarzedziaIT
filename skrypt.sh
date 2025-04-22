fdd28137ecdd8b2e660f79d202259ea03783e2db
Michał <kowalewskim444@gmail.com>
2025-04-22 15:48:03 +0200

    Dodanie flag z pojedynczym myslnikiem

--- skrypt.sh
+++ skrypt.sh
@@ -4,6 +4,11 @@ if [[ $1 == "--date" ]]; then
 	echo $(date)
 fi
 
+if [[ $1 == "-d" ]]; then
+        echo $(date)
+fi
+
+
 i=1
 if [[ $1 == "--logs" ]] && [[ $2 == "" ]]; then
 	while [ $i -le 100 ]
@@ -16,6 +21,18 @@ if [[ $1 == "--logs" ]] && [[ $2 == "" ]]; then
 	done
 fi
 
+if [[ $1 == "-l" ]] && [[ $2 == "" ]]; then
+        while [ $i -le 100 ]
+        do
+                touch "log$i.txt"
+                echo "log$i.txt" >> "log$i.txt"
+                echo "skrypt.sh" >> "log$i.txt"
+                echo $(date) >> "log$i.txt"
+                i=$(( $i + 1 ))
+        done
+fi
+
+
 if [[ $1 == "--logs" ]] && [[ -z "$2" ]]; then
 	while [ $i -le "$2" ]
 	do
@@ -27,11 +44,33 @@ if [[ $1 == "--logs" ]] && [[ -z "$2" ]]; then
 	done
 fi
 
-if [[ $1 == "--help" ]]; then
+if [[ $1 == "-l" ]] && [[ -z "$2" ]]; then
+        while [ $i -le "$2" ]
+        do
+                touch "log$i.txt"
+                echo "log$i.txt" >> "log$i.txt"
+                echo "skrypt.sh" >> "logs$i.txt"
+                echo $(date) >> "logs$i.txt"
+                i=$(( $i + 1 ))
+        done
+fi
+
+
+
+
+if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
 	echo "Wszystkie mozliwe opcje wykonania skryptu: "
 	echo "--date   (wyswietla dziejsza date)"
 	echo "--logs   (tworzy 100 plikow logx.txt)"
 	echo "--logs [nr]   (tworzy okreslona liczbe plikow logx.txt)"
 	echo "--help   (wyswietla te strone)"
 fi
+
+
+
+
+
+
+
+
 
