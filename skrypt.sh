2fea87123555d67a92039035cbb4e36a283892fc
Michał <kowalewskim444@gmail.com>
2025-04-22 15:30:06 +0200

    Dodanie --logs z drugim parametrem

--- skrypt.sh
+++ skrypt.sh
@@ -15,3 +15,14 @@ if [[ $1 == "--logs" ]] && [[ $2 == "" ]]; then
 		i=$(( $i + 1 ))
 	done
 fi
+
+if [[ $1 == "--logs" ]] && [[ -z "$2" ]]; then
+	while [ $i -le "$2" ]
+	do
+		touch "log$i.txt"
+		echo "log$i.txt" >> "log$i.txt"
+		echo "skrypt.sh" >> "logs$i.txt"
+		echo $(date) >> "logs$i.txt"
+		i=$(( $i + 1 ))
+	done
+fi
